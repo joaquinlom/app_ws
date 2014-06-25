d45311bd20578e34f02f19a7b55e771589b4ab14
joaquin <jvaldez.lom@gmail.com>
2014-06-25 16:31:27 +0000

    asd

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 var app = require('express')();
-var util = require("util");
 var http = require('http').Server(app);
-var io = require("socket.io");
-var socket;
-var player;
+var io = require('socket.io')(http);
+var players;
     Player = require("./Player").Player;
 
 var setEventHandlers = function() {
-    socket.sockets.on("connection", onSocketConnection);
-};
-function onSocketConnection(client) {
-   util.log("New player has connected: "+client.id);
-    socket.emit('Se conecto nueva persona');
-    client.on("disconnect", onClientDisconnect);
-    client.on("new player", onNewPlayer);
-    client.on("move player", onMovePlayer);
+    io.on('connection', function(socket){
+            console.log('a user connected'+socket.id);
+            socket.on("disconnect", onClientDisconnect);
+            socket.on("new player", onNewPlayer);
+            socket.on("move player", onMovePlayer);
+            socket.on("chat message",onChatMessage);
+        });
 };
 
 function onClientDisconnect() {
-    io.emit("Player has disconnected: "+this.id);
+    this.broadcast.emit("Player has disconnected: "+this.id);
 };
 
+function onChatMessage(msg){
+  io.emit('chat message', msg);
+};
 function onNewPlayer(data) {
   var newPlayer = new Player(data.x, data.y);
       newPlayer.id = this.id;
@@ -49,35 +49,13 @@ app.get('/', function(req, res){
 });
 
 var port = Number(process.env.PORT || 3000);
-socket = io.listen(port);
-/*
 http.listen(port, function(){
   console.log('listening on *:3000');
 });
-*/
+
 init();
 
-/*
-io.on('connection', function(socket){
-  console.log('a user connected');
-});
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-});
-
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-  });
-});
 
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
-*/
+
+
