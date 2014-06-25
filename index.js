c06a58ba97b841ba10bdf1e36f1ac7663ad249d2
joaquin <jvaldez.lom@gmail.com>
2014-06-25 15:52:30 +0000

    player class

--- index.js
+++ index.js
@@ -1,11 +1,62 @@
 var app = require('express')();
+var util = require("util");
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require("socket.io");
+var socket;
+var player;
+    Player = require("./Player").Player;
+
+var setEventHandlers = function() {
+    socket.sockets.on("connection", onSocketConnection);
+};
+function onSocketConnection(client) {
+   util.log("New player has connected: "+client.id);
+    client.on("disconnect", onClientDisconnect);
+    client.on("new player", onNewPlayer);
+    client.on("move player", onMovePlayer);
+};
+
+function onClientDisconnect() {
+    io.emit("Player has disconnected: "+this.id);
+};
+
+function onNewPlayer(data) {
+  var newPlayer = new Player(data.x, data.y);
+      newPlayer.id = this.id;
+  this.broadcast.emit("new player", {id: newPlayer.id, x: newPlayer.getX(), y: newPlayer.getY()});
+  
+    var i, existingPlayer;
+    for (i = 0; i < players.length; i++) {
+      existingPlayer = players[i];
+      this.emit("new player", {id: existingPlayer.id, x: existingPlayer.getX(), y: existingPlayer.getY()});
+    };
+    players.push(newPlayer);
+};
+
+function onMovePlayer(data) {
+
+};
+
+
+function init() {
+    players = [];
+    setEventHandlers();
+};
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
+var port = Number(process.env.PORT || 3000);
+socket = io.listen(port);
+/*
+http.listen(port, function(){
+  console.log('listening on *:3000');
+});
+*/
+init();
+
+/*
 io.on('connection', function(socket){
   console.log('a user connected');
 });
@@ -28,8 +79,4 @@ io.on('connection', function(socket){
     io.emit('chat message', msg);
   });
 });
-
-var port = Number(process.env.PORT || 5000);
-http.listen(port, function(){
-  console.log('listening on *:3000');
-});
+*/
